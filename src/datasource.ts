10a0dc7bc56a8ccf8f01ca4d37273d43f0b35cbe
karbasi <hamed.karbasi.93@gmail.com>
2021-10-07 13:13:20 +0330

    simple sin wave

---
+++ src/datasource.ts
@@ -0,0 +1,55 @@
+import defaults from 'lodash/defaults';
+
+import {
+  DataQueryRequest,
+  DataQueryResponse,
+  DataSourceApi,
+  DataSourceInstanceSettings,
+  MutableDataFrame,
+  FieldType,
+} from '@grafana/data';
+
+import { MyQuery, MyDataSourceOptions, defaultQuery } from './types';
+
+export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
+  constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
+    super(instanceSettings);
+  }
+
+  async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
+    const { range } = options;
+    const from = range!.from.valueOf();
+    const to = range!.to.valueOf();
+    // duration of the time range, in milliseconds.
+    const duration = to - from;
+    // step determines how close in time (ms) the points will be to each other.
+    const step = duration / 1000;
+    // Return a constant for each query.
+    const data = options.targets.map(target => {
+      const query = defaults(target, defaultQuery);
+      const frame = new MutableDataFrame({
+        refId: query.refId,
+        fields: [
+          // { name: 'Time', values: [from, to], type: FieldType.time },
+          // { name: 'Value', values: [query.constant, query.constant], type: FieldType.number },
+          { name: 'time', type: FieldType.time },
+          { name: 'value', type: FieldType.number },
+        ],
+      });
+      for (let t = 0; t < duration; t += step) {
+        frame.add({ time: from + t, value: Math.sin((2 * Math.PI * t) / duration) });
+      }
+      return frame;
+    });
+
+    return { data };
+  }
+
+  async testDatasource() {
+    // Implement a health check for your data source.
+    return {
+      status: 'success',
+      message: 'Success',
+    };
+  }
+}
