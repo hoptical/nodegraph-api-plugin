ce6c67d0163110943c067c3cce3f86a644e41f37
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2022-09-26 18:03:45 +0200

    resync-builder

--- src/datasource.ts
+++ src/datasource.ts
@@ -61,23 +61,27 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
           }
           // add single external link for items (link__url, link__title)
           if ('link__url' in field) {
-            outputField.config.links = [{
-              url: field['link__url'],
-              title: field['link__title'] || "Link",
-              targetBlank: true,
-            }];
+            outputField.config.links = [
+              {
+                url: field['link__url'],
+                title: field['link__title'] || 'Link',
+                targetBlank: true,
+              },
+            ];
           }
           // add single internal link for items (link__expr, link__uid, link__name)
-          if (('link__expr' in field) && ('link__uid' in field)) {
-            outputField.config.links = [{
-              url:  field['link__url'] || "",
-              title: field['link__title'] || "Link",
-              "internal": {
-              "query": {"expr": field['link__expr']},
-              "datasourceUid": field['link__uid'],
-              "datasourceName": field['link__name'],
-              }
-            }];
+          if ('link__expr' in field && 'link__uid' in field) {
+            outputField.config.links = [
+              {
+                url: field['link__url'] || '',
+                title: field['link__title'] || 'Link',
+                internal: {
+                  query: { expr: field['link__expr'] },
+                  datasourceUid: field['link__uid'],
+                  datasourceName: field['link__name'],
+                },
+              },
+            ];
           }
           outputFields.push(outputField);
         });
